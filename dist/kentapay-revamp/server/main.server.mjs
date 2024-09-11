288a3221410be3340b7ec87b5c24c91227d4c9d0
David Nyongo <njugunad85@gmail.com>
2024-09-11 12:58:13 +0300

    Automated slider
    Signed-off-by: David Nyongo <njugunad85@gmail.com>

--- dist/kentapay-revamp/server/main.server.mjs
+++ dist/kentapay-revamp/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SA6O3RPH.mjs";import"./chunk-SYHHE5RO.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-STL5PZ3Y.mjs";import"./chunk-CCQ4LFZM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
