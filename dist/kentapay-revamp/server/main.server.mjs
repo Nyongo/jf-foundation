518ae3f08287217ebb44c6954d58045362c05175
David Nyongo <njugunad85@gmail.com>
2024-09-11 08:31:38 +0300

    tailwind fix
    Signed-off-by: David Nyongo <njugunad85@gmail.com>

--- dist/kentapay-revamp/server/main.server.mjs
+++ dist/kentapay-revamp/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ERME4R3G.mjs";import"./chunk-4Q7ZMDJP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-A623XDJF.mjs";import"./chunk-OLIGU5FD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
