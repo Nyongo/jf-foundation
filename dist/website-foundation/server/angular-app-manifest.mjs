7b8ef5e17521b3dce23827ca4678efa320b1ff1b
David Nyongo <njugunad85@gmail.com>
2025-03-17 14:29:20 +0300

    fixed issues
    Signed-off-by: David Nyongo <njugunad85@gmail.com>

--- dist/website-foundation/server/angular-app-manifest.mjs
+++ dist/website-foundation/server/angular-app-manifest.mjs
@@ -9,6 +9,6 @@ export default {
   assets: {
     'index.csr.html': {size: 3975, hash: '36eaa60d451ab12cadcb770c1264bc8de304503ec39f0710141698d59dcb6d03', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 2343, hash: '599bb20cf9b31080f066b9a68e9ff4a87cff8926145a603d1dfedda81b77e5e5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles.css': {size: 43614, hash: '/OEeEcO7V1Y', text: () => import('./assets-chunks/styles_css.mjs').then(m => m.default)}
+    'styles.css': {size: 47194, hash: '8trxDPvPb/Q', text: () => import('./assets-chunks/styles_css.mjs').then(m => m.default)}
   },
 };
